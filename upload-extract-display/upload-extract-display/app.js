45abf83bd2456d0e4f7342c75bd7397798e44ed9
Zeyad Naguib <82226885+zeyadmtn@users.noreply.github.com>
2022-06-25 11:04:53 +0200

    Added zip images to display

--- upload-extract-display/upload-extract-display/app.js
+++ upload-extract-display/upload-extract-display/app.js
@@ -1,28 +1,36 @@
+const inpFile = document.querySelector("#inpFile");
+const imagesDiv = document.querySelector('.images-container');
+
 (function test() {
-  const inpFile = document.querySelector("#inpFile");
+
 
 
   inpFile.addEventListener("change", (e) => {
 
 
-    var files = [];
-    
-
-
-    var jsZip = new JSZip();
-    jsZip.loadAsync(inpFile.files[0]).then(function (zip) {
-        Object.keys(zip.files).forEach(function (filename) {
-          zip.files[filename].async('ArrayBuffer').then(function (fileData) {
-            var arrayBufferView = new Uint8Array(fileData);
-            var blob = new Blob( [ arrayBufferView ], { type: "image/jpeg" } );
-            var urlCreator = window.URL || window.webkitURL;
-            files.push(urlCreator.createObjectURL( blob ));
-            console.log(files);
+        var jsZip = new JSZip();
+ 
+        jsZip.loadAsync(inpFile.files[0]).then(function (zip) {
+            Object.keys(zip.files).forEach(function (filename) {
+              zip.files[filename].async('ArrayBuffer').then(function (fileData) {
+                var arrayBufferView = new Uint8Array(fileData);
+                var blob = new Blob( [ arrayBufferView ], { type: "image/jpeg" } );
+                var urlCreator = window.URL || window.webkitURL;
+                url = urlCreator.createObjectURL( blob );
+                displayImg(url);
+                
+  
+              })
+            })
+            
           })
-        })
-      })
-
-      
 
   });
 })();
+
+ const displayImg = (url) => {
+    let imgElement = document.createElement("img");
+    imgElement.className = 'imgElement';
+    imgElement.src = url;
+    imagesDiv.appendChild(imgElement);
+}
